be204d31d129f7b1f3faa243edfacbd1c1dc1a52
andreaswolff <wandrease@gmail.com>
2021-07-02 21:59:46 +0300

    changed carousels

--- src/js/landing.js
+++ src/js/landing.js
@@ -54,19 +54,19 @@ const buildModal = (video, info) => {
     <iframe loading="lazy" class="w-full h-96" src="https://www.youtube.com/embed/${video}" title="YouTube video player" frameborder="0"  allowfullscreen></iframe>
 
     <div class="my-2 pl-4 flex pb-5">
-    <div class="flex flex-col justify-center">
-      <div class="flex items-center mb-5">
-      <div class="w-1 h-12 lg:h-10 bg-purple-500 mr-2 mt-2"></div>
-      <p class="font-bold text-3xl text-white pt-2">${info.title}</p>
+    <div class="flex flex-col justify-center w-3/4 ">
+      <div class="flex items-center mb-5 relative flex-none">
+      <div class="absolute w-1 h-full bg-purple-500 mr-2 mt-2"></div>
+      <p class="font-bold text-3xl text-white pt-2 ml-4">${info.title}</p>
      
     </div>
-    <p class=" text-sm text-white text-opacity-70 w-3/4 ">${info.overview}</p>
+    <p class=" text-sm text-white text-opacity-70 w-full line-clamp-6">${info.overview}</p>
     </div>
 
-    <div class="flex flex-col pl-1">
+    <div class="flex flex-col px-2 pt-3 w-2/4 mr-4">
     <p class="text-sm text-white"><span class="font-bold text-purple-400">Genre: </span>${info.genres[0].name}</p>
     <p class="font-bold text-sm text-purple-400 mb-5">${info.popularity}% popularity</p>
-    <p class=" text-sm text-white text-opacity-70"><span class="font-bold text-md text-purple-400">Cast:</span> Tom Ellis, Lauren German, Kevin Alejandro, D.B. Woodside, Lesley-Ann Brandt, Rachael Harris, more</p>
+    <p class=" text-sm text-white text-opacity-70 pt-1"><span class="font-bold text-md text-purple-400">Cast:</span> Tom Ellis, Lauren German, Kevin Alejandro, D.B. Woodside, Lesley-Ann Brandt, Rachael Harris, more</p>
   </div>
     
     
@@ -92,35 +92,150 @@ document.addEventListener("click", (e) => {
 /////////
 /////////
 
-let gliderOptions = {
-  slidesToScroll: 1,
-  slidesToShow: 6,
-  draggable: true,
-  resizeLock: true,
-  scrollLock: true,
-  rebuildPaging: true,
-  duration: 2,
-  arrows: {
-    prev: ".glider-prev",
-    next: ".glider-next",
-  },
-};
-
-let gliderOptionsVideosBackdrop = {
-  slidesToScroll: 1,
-  slidesToShow: 4,
-  draggable: true,
-  resizeLock: true,
-  scrollLock: true,
-  duration: 2,
-  arrows: {
-    prev: ".glider-prev",
-    next: ".glider-next",
-  },
-};
-
 // TRENDING CAROUSEL
 let filterArg = ["day"];
+
+const swiper1 = new Swiper(".swiper1", {
+  // Optional parameters
+  freeModeSticky: true,
+  allowSlideNext: true,
+  allowSlidePrev: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+  },
+
+  a11y: {
+    prevSlideMessage: "Previous slide",
+    nextSlideMessage: "Next slide",
+  },
+
+  mousewheel: {
+    invert: false,
+  },
+});
+const swiper2 = new Swiper(".swiper2", {
+  // Optional parameters
+  freeModeSticky: true,
+  allowSlideNext: true,
+  allowSlidePrev: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+  },
+
+  a11y: {
+    prevSlideMessage: "Previous slide",
+    nextSlideMessage: "Next slide",
+  },
+
+  mousewheel: {
+    invert: false,
+  },
+});
+const swiper3 = new Swiper(".swiper3", {
+  // Optional parameters
+  freeModeSticky: true,
+
+  allowSlideNext: true,
+  allowSlidePrev: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  },
+
+  a11y: {
+    prevSlideMessage: "Previous slide",
+    nextSlideMessage: "Next slide",
+  },
+
+  mousewheel: {
+    invert: false,
+  },
+});
 
 trendFilter.addEventListener("click", (e) => {
   if (!e.target.dataset.trend) return;
@@ -171,19 +286,23 @@ function carouselOne(movies) {
   const html = movies
     .map((movie) => {
       return `
-        <div class="mr-2" data-id="${movie.id}" id="movietrending">
-        <img loading="lazy"   src="${IMAGE_URL}${movie.poster_path}" alt="${movie.title}" class=" bg-stretch object-cover hover:cursor-pointer hover:shadow-lg">
       
-      </img>
+      <div class="swiper-slide" >
+        
+      <img loading="lazy" src="${IMAGE_URL}${movie.poster_path}" alt="${movie.title}" data-id="${movie.id}" id="movietrending" class="bg-stretch object-cover hover:cursor-pointer hover:shadow-lg  w-50 h-auto">
+    </img>
 
-      
+    
 
-        </div>
+      </div>
+    
+        
         `;
     })
     .join("");
   trendingContainer.insertAdjacentHTML("beforeend", html);
-  new Glider(document.querySelector(".trendingNow"), gliderOptions);
+  swiper1.update();
+  // new Glider(document.querySelector(".trendingNow"), gliderOptions);
 }
 
 // PEOPLE CAROUSEL
@@ -204,57 +323,30 @@ function carouselTwo(actors) {
   const html = actors
     .map((actor) => {
       return `
-        <div class="mr-2">
-        <img loading="lazy"  src="${IMAGE_URL}${actor.profile_path}" alt="${actor.name}" class=" hover:cursor-pointer hover:shadow-lg" />
+        <div class="swiper-slide">
+        <img loading="lazy"  src="${IMAGE_URL}${actor.profile_path}" alt="${actor.name}" class=" hover:cursor-pointer hover:shadow-lg w-50 h-auto" />
         <p class="relative text-white overflow-hidden bg-purple-500 bg-opacity-90 uppercase text-xs font-bold text-center py-2 px-1 truncate">${actor.name}</p>
         </div>
         `;
     })
     .join("");
   popularPeople.insertAdjacentHTML("beforeend", html);
-  new Glider(document.querySelector(".popularPeople"), gliderOptions);
+  swiper2.update();
+  // new Glider(document.querySelector(".popularPeople"), gliderOptions);
 }
-
-//TRAILER
-//TRAILER
-//TRAILER
-
-// function fetchTrailers(movieData) {
-//   movieData.forEach((movie) => {
-//     fetch(
-//       `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=1f4df7f17529b542876a985507f244b0&language=en-US`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => displayVids(data.results));
-//   });
-// }
-
-// document.addEventListener("click", (e) => {
-//   if (e.target.id !== "clickedmovie") return;
-
-//   getMovieTrailer(e.target.dataset?.id)
-//     .then((res) => {
-//       if (!res.results || !res.results[0])
-//         throw new Error(
-//           "We were unable to get any data for that specific movie. Try again."
-//         );
-//       getMovieInfo(res.id).then((data) => buildModal(res.results[0].key, data));
-//     })
-//     .catch((err) => {
-//       backdropMovieInfo.innerHTML = `<p class="flex justify-center items-center text-white m-auto p-4 font-bold text-xl">${err.message}</p>`;
-//     });
-// });
 
 function displayVids(vidId, trailer) {
   console.log(vidId, trailer);
   const html = vidId
     .map((video) => {
       return `
-      <div class="mr-2 h-full hover:scale-90 drop-shadow-md filter duration-200">
-      <div class="relative group" data-id=${video.id} id="clickedmovie">
+    <div class="swiper-slide hover:scale-90  drop-shadow-md filter duration-200 ">
+      <div class="group relative">
         <img loading="lazy" src="${IMAGE_URL}${video.backdrop_path}" alt="${
         video.title
-      }" class="group-hover:grayscale z-10 hover:cursor-pointer hover:shadow-lg border-2 border-purple-500">
+      }" class="group-hover:grayscale z-10 hover:cursor-pointer hover:shadow-lg border-2 border-purple-500" data-id=${
+        video.id
+      } id="clickedmovie">
         </img>
         <i class="fas fa-play text-white text-3xl absolute pb-5 z-30 bottom-2/4 top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 group-hover:text-purple-400 group-hover:scale-150 duration-200"></i>
         </div>
@@ -266,5 +358,6 @@ function displayVids(vidId, trailer) {
     .join("");
 
   trailerContainer.insertAdjacentHTML("beforeend", html);
-  new Glider(document.querySelector(".trailers"), gliderOptionsVideosBackdrop);
+  swiper3.update();
+  // new Glider(document.querySelector(".trailers"), gliderOptionsVideosBackdrop);
 }
